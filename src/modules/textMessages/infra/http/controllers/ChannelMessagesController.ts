719cf28233ca0cbfe7c21ad04c19ef3e6dc24ac5
Pedro Omar <pedromarneto@hotmail.com>
2021-08-18 23:10:22 -0300

    feat: developing eye reminders

--- src/modules/textMessages/infra/http/controllers/ChannelMessagesController.ts
+++ src/modules/textMessages/infra/http/controllers/ChannelMessagesController.ts
@@ -1,23 +1,29 @@
 import { container } from 'tsyringe'
-import { Message, MessageOptions } from 'discord.js'
+import { Message, MessageOptions, User } from 'discord.js'
 
 import GetRandomMemeImageService from '@modules/memes/services/GetRandomMemeImageService'
 
 export default class ChannelMessagesController {
   private memeMessage = 0
+  private eyeBreakSubscribers: User[] = []
+  private interval: NodeJS.Timer
 
   public async execute(lastMessage: Message): Promise<void> {
     switch (lastMessage.content) {
       case '!meme':
         this.memeRequestFlow(lastMessage)
         break
+      case '!eye':
+        this.eyeBreakReminderRequestFlow(lastMessage)
+        break
+      case '!stop':
+        this.eyeBreakReminderStopFlow(lastMessage)
+        break
     }
   }
 
   private async memeRequestFlow(lastMessage: Message) {
     this.memeMessage++
-
-    console.log('memeMessage: ', this.memeMessage)
 
     this.sendHeresYourMemeMessage(lastMessage)
 
@@ -44,5 +50,65 @@ export default class ChannelMessagesController {
     lastMessage.channel.send(<MessageOptions>randomMemeImage)
 
     if (this.memeMessage > 2) lastMessage.channel.send(`:rage: :rage: :rage:`)
+  }
+
+  private async eyeBreakReminderRequestFlow(lastMessage: Message) {
+    await this.sendYouAreSubscribeInEyeBreakMessage(lastMessage)
+  }
+
+  private async sendYouAreSubscribeInEyeBreakMessage(lastMessage: Message) {
+    const checkUserExistsInEyeBreakReminder = this.eyeBreakSubscribers.find(
+      user => user.id === lastMessage.author.id,
+    )
+
+    console.dir(this.eyeBreakSubscribers, { depth: null })
+
+    if (!!checkUserExistsInEyeBreakReminder)
+      lastMessage.channel.send(
+        'User already subscribed in reminder! For exit send "!stop"',
+      )
+    else {
+      this.createIntervalEyeBreak(lastMessage)
+
+      const textMessage = `You are now subscribed to eye break reminders`
+
+      lastMessage.channel.send(textMessage)
+
+      this.eyeBreakSubscribers.push(lastMessage.author)
+    }
+  }
+
+  private createIntervalEyeBreak(lastMessage: Message) {
+    this.interval = setInterval(function () {
+      lastMessage.channel
+        .send('Please take an eye break now!')
+        .catch(console.error)
+    }, 10000)
+  }
+
+  private async eyeBreakReminderStopFlow(lastMessage: Message) {
+    const findUserIndexInEyeBreakReminder = this.eyeBreakSubscribers.findIndex(
+      user => user.id === lastMessage.author.id,
+    )
+
+    console.dir(this.eyeBreakSubscribers, { depth: null })
+
+    if (findUserIndexInEyeBreakReminder !== -1)
+      lastMessage.channel.send(
+        'User are unsubscribed in reminder! For enter send "!eye"',
+      )
+    else {
+      this.removeIntervalEyeBreak()
+
+      const textMessage = `I have stopped eye reminders`
+
+      lastMessage.channel.send(textMessage)
+
+      this.eyeBreakSubscribers.splice(findUserIndexInEyeBreakReminder, 1)
+    }
+  }
+
+  private removeIntervalEyeBreak() {
+    clearInterval(this.interval)
   }
 }
